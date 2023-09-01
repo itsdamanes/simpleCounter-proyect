91dfc93a5b732f046be61035d3d9b34ddc8cc771
Ignacio <ignacio.damanes@gmail.com>
2023-09-01 01:50:38 -0400

    avance funcion SECOND

--- src/js/index.js
+++ src/js/index.js
@@ -11,3 +11,14 @@ import Home from "./component/home.jsx";
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
 
+const counterBoxes = document.querySelector('.digit-box');
+
+let second = 0;
+
+function updateDigitCounter() {
+    const digit = second.toString().padStart(6, '0');
+
+    for (let i = 0; i < second.length; i++) {
+        second[i].textContent = digit[i];
+    }
+}
